aa026bad06da745c8f7e20ae4fd20f919b94ba8a
junsudada <js7946130@naver.com>
2025-06-21 22:55:36 +0900

    Title: 달에서 무게 구하기, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250621/달에서 무게 구하기/weight-on-the-moon.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+
+    int weight=13;
+    double rate=0.165;
+    cout << fixed;
+    cout.precision(6);
+    cout<< weight<< " * "<<rate<<" = "<<weight*rate;
+
+
+    return 0;
+}
